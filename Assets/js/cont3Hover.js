15ded2e66f496e29b19d1867925aff9b877d8750
GarngbelrLurain <pcaso5@naver.com>
2020-10-13 17:02:03 +0900

    Add files via upload

---
+++ Assets/js/cont3Hover.js
@@ -0,0 +1,25 @@
+const shopBox = document.querySelectorAll(".shop_box>div")
+const shopBoxText = document.querySelectorAll(".cont3_text>p")
+const shopBoxImg = document.querySelector("#cont3_img")
+
+function shopBoxTextOn(){
+	const selector = this
+	
+	shopBox.forEach(function(elem, index){
+		if(elem == selector){
+			shopBoxText[index].style.opacity = 1;
+			const indexString = String(index + 1);
+			shopBoxImg.classList.add("cont3_img"+indexString)
+		} else {
+			shopBoxText[index].style.opacity = 0;
+			const indexStringNo = String(index + 1);
+			shopBoxImg.classList.remove("cont3_img"+indexStringNo);
+		}
+	})
+	console.log()
+	
+}
+
+for(const shopBoxBtn of shopBox){
+	shopBoxBtn.addEventListener("mouseover", shopBoxTextOn)
+}
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-code update (2020.10.06 - refactoring html tag & css(too many use flex, background re-peat))</br>
 code upload (2020.10.05)</br>
 design upload (2020.10.02)</br>
 https://github.com/GrangbelrLurain/20-09_P1/issues/1
