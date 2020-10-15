bb0d3deae5abd3433faaa4aab9523cec0c45c0b9
김규연 <pcaso5@naver.com>
2020-10-15 18:16:55 +0900

    layerPopUpChanges : layer select

--- Assets/js/layerPopUp.js
+++ Assets/js/layerPopUp.js
@@ -1,12 +1,29 @@
 // Layer Pop Up On
 const layerOnBtn = document.querySelectorAll(".cont4>div>div>div")
+const layers = document.querySelectorAll("#layer>article")
 
-function layerOn (){
-    const layer = document.getElementById("layer");
-    layer.style.display = "flex";
+function chooseLayer(selector){
+    
+    layerOnBtn.forEach(function(Elem, index){
+        if(selector == Elem){
+            layers[index].style.display = "flex";
+            
+        }else{
+            layers[index].style.display = "none";
+        }
+    })
 }
 
-layerOnBtn.forEach(function(Elem){
+function layerOn (){
+    const selector = this;
+
+    const layer = document.getElementById("layer");
+    layer.style.display = "flex";
+
+    chooseLayer(selector);
+}
+
+layerOnBtn.forEach(function(Elem, index){
     Elem.addEventListener("click", layerOn);
 });
 
@@ -14,9 +31,17 @@ layerOnBtn.forEach(function(Elem){
 // Layer Pop Up EXIT
 const layerExitBtn = document.querySelectorAll(".layer_exit");
 
+function everyLayerOff(){
+    layers.forEach(function(Elem, index){
+        Elem.style.display = "none";
+    })
+}
+
 function layerExit (){
     const layer = document.getElementById("layer");
     layer.style.display = "none";
+
+    everyLayerOff();
 }
 
 layerExitBtn.forEach(function (Elem){
